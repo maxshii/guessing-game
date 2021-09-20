1ac3fd50713ef7c383224f46e06deaa32d56b9f5
U-BSD\405251 <405251@SHS-10D9C063E.beaverton.k12.or.us>
2021-09-20 12:09:35 -0700

    finished game

--- game.cpp
+++ game.cpp
@@ -4,13 +4,52 @@
 #include <stdlib.h>
 #include <time.h>
 
+using namespace std;
+
 int main()
 {
   srand(time(NULL));
   bool play = true;
-  
+
+  //play the game until player answers no
   while(play == true)
     {
-      int number = rand() % 1
+      int number = rand() % 101; //the number that the player is trying to guess
+      int guess = 200; //the number guessed by the player
+      int guessNum = 0; //number of guesses
+      //cout << number;
+      
+      //checks if the guess is greater than, less than, or equal to the number
+      cout << "Guess a number: ";
+      while(guess != number)
+	{
+	  guessNum++;
+	  cin >> guess;
+	  if
+	  
+	  if(guess < number)
+	    {
+	      cout << "Your guess is too low. Guess again: ";
+	    }
+	  else if(guess > number)
+	    {
+	      cout << "Your guess is too high. Guess again: ";
+	    } 
+	}
+
+      //displays end information and asks if the player wants to play again
+      cout << "Your guess is correct. Total guesses:" << guessNum << endl << "Play again? (y/n)";
+      char in;
+      cin >> in;
+      if(in == 'n')
+	{
+	  cout << "Game over.";
+          play = false;
+	}
+      else if(in != 'y')
+	{
+	  cout << "Enter y or n.";
+	}
+        
     }
 }
