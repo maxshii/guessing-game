26fe6855a302ce49fc534c4771a2061c0c99c7f5
U-BSD\405251 <405251@SHS-10D9C063E.beaverton.k12.or.us>
2021-09-20 10:57:56 -0700

    second commit

---
+++ game.cpp
@@ -0,0 +1,16 @@
+//This program will generate a number between 0 and 100. The user will guess a number.
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  srand(time(NULL));
+  bool play = true;
+  
+  while(play == true)
+    {
+      int number = rand() % 1
+    }
+}
